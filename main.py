bc97cff79e8d4c8ce97bf7a41f062a4fd782acd6
Douglas Mendes <douglasleomendes@gmail.com>
2019-04-18 11:58:54 -0300

    Codigo reestruturado e criado menu para o usuario escolher se deseja mostrar o catálogo ou adicionar um novo filme

--- main.py
+++ main.py
@@ -36,9 +36,26 @@ def cria_filme(conn,filme):
     cur.execute(sql, filme)
     return cur.lastrowid
 
+def seleciona_filmes(conn):
+    """
+    Busca todas as linhas na tabela de filmes
+    :paramentro conn:Objeto de conexão
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM filmes")
+
+    rows = cur.fetchall()
+    print("==================Catálogo================")
+
+    for row in rows:
+        print(row)
+    print("==========================================")
+
 def main():
+    #base de dados que utilizaremos
     database = 'pythonsqlite.db'
-    #variavel recebe o script para criação da tabela
+    #variavel recebe o script para criação da tabela de filmes
     sql_cria_tabela_filmes = """ CREATE TABLE IF NOT EXISTS filmes (
                                         id integer PRIMARY KEY,
                                         nome text NOT NULL,
@@ -55,14 +72,23 @@ def main():
         print("Erro ! Não foi possivel estabelecer uma conexão com a base de dados")
 
     with conn:
-        #cria novo filme
-        nome = input("Digite o nome do filme:\n")
-        data_lancamento = input("Digite a data do filme(yyyy-mm-dd):\n")
-        nota = input("Digite a nota do filme(0-10):\n")
-        filme = (nome, data_lancamento, nota)
-        filme_id = cria_filme(conn, filme)
-        print (filme_id)
-            
+        #Menu com as opções para o usuario
+        opcao = ""
+        while(opcao != 0):
+            print("++++++++++++++++MENU++++++++++++++++")
+            opcao = int(input("1-Mostrar catálogo\n2-Adicionar filme\n0-Sair\nDigite o codigo da opção desejada:"))
+            if opcao == 1:
+                seleciona_filmes(conn)
+            elif opcao == 2:
+                #cria novo filme
+                nome = input("Digite o nome do filme:\n")
+                data_lancamento = input("Digite a data do filme(yyyy-mm-dd):\n")
+                nota = input("Digite a nota do filme(0-10):\n")
+                filme = (nome, data_lancamento, nota)
+                filme_id = cria_filme(conn, filme)
+                print (filme_id)
+            else:
+                print("See Ya!")
 
 
 if __name__ =='__main__':
