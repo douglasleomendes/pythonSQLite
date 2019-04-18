3f33d042a7d9825edcf47461339716611f64a379
Douglas Mendes <douglasleomendes@gmail.com>
2019-04-18 10:33:43 -0300

    Codigo alterado para criar um catalogo de filmes com nome, data de lançamento e nota, onde o usuario digita os dados de entrada

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def create_connection(db_file):
 
 def create_table(conn, create_table_sql):
     """cria tabela 
-    :param conn: Connection object
+    :param conn: objeto conexao
     :param create_table_sql: a CREATE TABLE statement
     :return:
     """
@@ -22,79 +22,47 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def create_task(conn,task):
-    """
-    Cria nova tarefa
-    :param conn:
-    :param task:
-    :return:
-    """
-
-    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
-              VALUES(?,?,?,?,?,?) '''
-
-    cur = conn.cursor()
-    cur.execute(sql,task)
-    return cur.lastrowid
     
-def create_project(conn,project):
+def cria_filme(conn,filme):
     """
-    Cria um novo projeto na tabela de projetos
+    Cria um novo filme na tabela de filmes
     :param conn:
-    :param project:
-    :return: project id
+    :param filme:
+    :return: filme id
     """
-    sql ='''INSERT INTO projects(name,begin_date,end_date)
+    sql ='''INSERT INTO filmes(nome,data_lancamento,nota)
             VALUES(?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, filme)
     return cur.lastrowid
 
 def main():
     database = 'pythonsqlite.db'
-
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+    #variavel recebe o script para criação da tabela
+    sql_cria_tabela_filmes = """ CREATE TABLE IF NOT EXISTS filmes (
                                         id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
+                                        nome text NOT NULL,
+                                        data_lancamento text,
+                                        nota int
                                     ); """
-
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                                );"""
 
     #cria conexão com base de dados
     conn = create_connection(database)
     if conn is not None:
         #cria a tabela de projetos
-        create_table(conn, sql_create_projects_table)
-        #cria tabela de tarefas
-        create_table(conn, sql_create_tasks_table)
+        create_table(conn, sql_cria_tabela_filmes)
     else:
         print("Erro ! Não foi possivel estabelecer uma conexão com a base de dados")
 
-    opcao = input('Digite a opção desejada:\n1-Adicionar Projeto\n2-Adicionar Tarefa')
-    if opcao == 1:
-        with conn:
-            #cria novo projeto
-            project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
-            project_id = create_project(conn, project)
-            #tarefas
-            task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
-            task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
-            #cria tarefas
-            create_task(conn, task_1)
-            create_task(conn, task_2)
-
-
+    with conn:
+        #cria novo filme
+        nome = input("Digite o nome do filme:\n")
+        data_lancamento = input("Digite a data do filme(yyyy-mm-dd):\n")
+        nota = input("Digite a nota do filme(0-10):\n")
+        filme = (nome, data_lancamento, nota)
+        filme_id = cria_filme(conn, filme)
+        print (filme_id)
+            
 
 
 if __name__ =='__main__':
